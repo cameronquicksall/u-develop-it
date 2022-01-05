ccbc3dda9eb784019f7680516914993a391a8712
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-01-05 17:22:49 -0600

    update candidate route

--- server.js
+++ server.js
@@ -40,7 +40,12 @@ app.use(express.json());
 
     // Get a single candidate
     app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+        const sql = `SELECT candidates.*, parties.name 
+        AS party_name 
+        FROM candidates 
+        LEFT JOIN parties 
+        ON candidates.party_id = parties.id 
+        WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -90,8 +95,11 @@ app.use(express.json());
         return;
     }
 
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-        VALUES (?,?,?)`;
+        const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
     const params = [body.first_name, body.last_name, body.industry_connected];
 
     db.query(sql, params, (err, result) => {
